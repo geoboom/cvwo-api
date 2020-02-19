7c9e05cee376f6464392d1b076bf6173b456d956
geoboom <leegeorgie52@gmail.com>
2020-02-19 10:24:27 +0800

    config

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+mount ActionCable.server => "/cable"
+
 Rails.application.routes.draw do
   resources :test_models
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
